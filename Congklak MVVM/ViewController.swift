185f9b6387b87efcc77a5bbfff2bc881b29ab924
bot5005 <62124837+bot5005@users.noreply.github.com>
2022-02-19 10:19:44 +0700

    Initial Commit

---
+++ Congklak MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Congklak MVVM
+//
+//  Created by William Johanssen Hutama on 19/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
